f93e8ba809d8aaafbd6072802c900b27b7a33760
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-05-29 23:05:29 +0530

    updated app.py
    added limited quote function

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import *
 import json, time
 import random as rand
-from db import quote_fetcher
+from db import quote_fetcher, limited_quote_fetcher
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    # current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    # data_set = {'Page': 'Home', 'Message': "Success on home", 'Time': current_time}
-    # json_dump = json.dumps(data_set)
-    # return json_dump
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    data_set = {'Page': 'Home', 'Message': "Success on home", 'Time': current_time}
+    json_dump = json.dumps(data_set)
+    return json_dump
 
     return render_template('index.html')
 
@@ -19,8 +19,15 @@ def get_quote():
 
 
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    data_set = {'Page': 'Quote', 'Message': "Successfully landed on quotes page", 'quote': f'{quote_fetcher()}', 'details': 'This data is fetched from Mongo DB'}
+    data_set = {'Page': 'Quote', 'Message': "Successfully landed on quotes page", 'quotes': f'{quote_fetcher()}', 'Time': current_time}
     
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+@app.route('/limit/', methods=['GET'])
+def limit_quote_fetcher():
+    limited_value = str(request.args.get('limit'))
+    data_set = {'Status': 'Successful', 'Message': f'Results {limited_quote_fetcher(limited_value)}'}
     json_dump = json.dumps(data_set)
     return json_dump
 
