2f8e263b00f8f7e70bc9c3835e4ae1621e83d852
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-05-31 18:44:36 +0530

    clean codew

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 from flask import *
 import json, time
-import random as rand
-from db import quote_fetcher, limited_quote_fetcher
+import random as rand # Yes I knew, What I was doing :)
+from db import *
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    data_set = {'Page': 'Home', 'Message': "Success on home", 'Time': current_time}
-    json_dump = json.dumps(data_set)
-    return json_dump
-
     return render_template('index.html')
 
 @app.route('/quote/', methods=['GET'])
 def get_quote():
-
-
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
     data_set = {'Page': 'Quote', 'Message': "Successfully landed on quotes page", 'quotes': f'{quote_fetcher()}', 'Time': current_time}
-    
     json_dump = json.dumps(data_set)
     return json_dump
 
@@ -37,8 +29,8 @@ def user_page():
     user_query = str(request.args.get('user'))
     data_set = {'Page': 'Request', 'Message': f"Success on User for {user_query}", 'Time': current_time}
     json_dump = json.dumps(data_set)
-
     return json_dump
 
 if __name__ == '__main__':
     app.run(port=5600)
+# You can use default port ^
